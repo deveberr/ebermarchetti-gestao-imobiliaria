45dec5a7e5730b36a1a535b36d514bb0a07d5b10
deveberr <dev.eberr@gmail.com>
2023-11-29 17:07:54 -0300

    Header e introdução responsivos + menu hamburguer

--- home.js
+++ home.js
@@ -1,37 +1,14 @@
-// const campoBusca = document.getElementById("campo-busca");
-
-// const textoDesejado = "Digite a cidade, bairro ou código do imóvel...";
-// let larguraDesejada = textoDesejado.length - 10;
-// campoBusca.style.width = larguraDesejada + "ch";
-
-// function atualizarLarguraCampoBusca() {
-//   if (window.innerWidth <= 600) {
-//     larguraDesejada = textoDesejado.length - 15;
-//   } else {
-//     larguraDesejada = textoDesejado.length - 10;
-//   }
-// }
-
-// window.addEventListener("resize", function () {
-//   campoBusca;
-//   if (window.innerWidth <= 600) {
-//     campoBusca.placeholder = "Busque por imóveis...";
-//   } else {
-//     campoBusca.placeholder = textoDesejado;
-//   }
-// });
-
 const campoBusca = document.getElementById("campo-busca");
 
-const textoDesejado = "Digite a cidade, bairro ou código do imóvel...";
+const textoDesejado = "Digite o código do imóvel...";
 let larguraDesejada = textoDesejado.length - 10;
 
 // Função para verificar e atualizar a largura do campo de busca
 function atualizarLarguraCampoBusca() {
   if (window.innerWidth <= 600) {
-    larguraDesejada = textoDesejado.length - 10; // Altere o valor desejado
+    larguraDesejada = textoDesejado.length - 6; // Altere o valor desejado
   } else {
-    larguraDesejada = textoDesejado.length - 10; // Valor padrão
+    larguraDesejada = textoDesejado.length - 6; // Valor padrão
   }
   campoBusca.style.width = larguraDesejada + "ch";
 }
@@ -40,15 +17,14 @@ function atualizarLarguraCampoBusca() {
 window.addEventListener("load", atualizarLarguraCampoBusca);
 window.addEventListener("resize", atualizarLarguraCampoBusca);
 
-// Função para verificar e atualizar o placeholder
-function atualizarPlaceholderCampoBusca() {
-  if (window.innerWidth <= 600) {
-    campoBusca.placeholder = "Digite o código do imóvel";
-  } else {
-    campoBusca.placeholder = textoDesejado;
-  }
+// Header mobile
+const btnMobile = document.getElementsByClassName("btn-mobile")[0];
+
+function toggleMenu(event) {
+  if (event.type === "touchstart") event.prevent.Default();
+  const nav = document.querySelector(".nav");
+  nav.classList.toggle("active");
 }
 
-// Adicione um ouvinte de evento para verificar a largura da tela
-window.addEventListener("load", atualizarPlaceholderCampoBusca);
-window.addEventListener("resize", atualizarPlaceholderCampoBusca);
+btnMobile.addEventListener("click", toggleMenu);
+btnMobile.addEventListener("touchstart", toggleMenu);
